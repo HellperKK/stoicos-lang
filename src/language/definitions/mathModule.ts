0c228127d9f0335d80158b83ae406a645e982d7a
HellperKK <baudetad@gmail.com>
2024-06-09 17:20:42 +0200

    add math module

---
+++ src/language/definitions/mathModule.ts
@@ -0,0 +1,93 @@
+import BaseToken from "../tokens/BaseToken";
+import FunToken from "../tokens/FunToken";
+import NumberToken from "../tokens/NumberToken";
+import StructToken from "../tokens/StructToken";
+
+const mathInit = () => {
+  const module = new Map<string, BaseToken>();
+
+  module.set(
+    'PI',
+    new NumberToken(Math.PI)
+  );
+  module.set(
+    'abs',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      return new NumberToken(Math.abs(num));
+    })
+  );
+  module.set(
+    'floor',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      return new NumberToken(Math.floor(num));
+    })
+  );
+  module.set(
+    'ceil',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      return new NumberToken(Math.ceil(num));
+    })
+  );
+  module.set(
+    'round',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      return new NumberToken(Math.round(num));
+    })
+  );
+  module.set(
+    'trunc',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      return new NumberToken(Math.trunc(num));
+    })
+  );
+
+  module.set(
+    'min',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      const numb = toks[1].request('number');
+      return new NumberToken(Math.min(num, numb));
+    })
+  );
+  module.set(
+    'max',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      const numb = toks[1].request('number');
+      return new NumberToken(Math.max(num, numb));
+    })
+  );
+  module.set(
+    'clamp',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      const min = toks[1].request('number');
+      const max = toks[2].request('number');
+      return new NumberToken(Math.min(Math.max(num, min), max));
+    })
+  );
+  module.set(
+    'pow',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      const numb = toks[1].request('number');
+      return new NumberToken(Math.pow(num, numb));
+    })
+  );
+  module.set(
+    'sqrt',
+    FunToken.native((toks) => {
+      const num = toks[0].request('number');
+      return new NumberToken(Math.sqrt(num));
+    })
+  );
+
+  return new StructToken(module);
+};
+
+export default mathInit;
