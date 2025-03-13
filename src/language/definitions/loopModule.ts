fda97af1b0c905ca4d4e498fdb900e4d4a6d5471
HellperKK <baudetad@gmail.com>
2025-03-13 18:38:25 +0100

    updated loops to ahve functionnal and imperative ones

--- src/language/definitions/loopModule.ts
+++ src/language/definitions/loopModule.ts
@@ -1,31 +1,48 @@
 /* eslint-disable no-plusplus */
-import BaseToken from '../tokens/BaseToken';
-import FunToken from '../tokens/FunToken';
-import StructToken from '../tokens/StructToken';
-import VarManager from '../manager/VarManager';
-import NumberToken from '../tokens/NumberToken';
+import BaseToken from "../tokens/BaseToken";
+import FunToken from "../tokens/FunToken";
+import StructToken from "../tokens/StructToken";
+import VarManager from "../manager/VarManager";
+import NumberToken from "../tokens/NumberToken";
 
 const loopInit = () => {
   const module = new Map<string, BaseToken>();
 
   // Functions
   module.set(
-    'while',
+    "while",
     FunToken.native((toks) => {
       const condition = toks[0];
       const body = toks[1];
-      while (condition.calculate().request('bool')) {
+      while (condition.calculate().request("bool")) {
         body.calculate();
       }
       return VarManager.unit;
     })
   );
   module.set(
-    'upto',
+    "upto",
     FunToken.native((toks) => {
-      const min = toks[0].request('number');
-      const max = toks[1].request('number');
-      const f = toks[2].request('fun');
+      const min = toks[0].request("number");
+      const max = toks[1].request("number");
+      const symbol = toks[2].request("symbol");
+      const body = toks[3];
+
+      const manager = VarManager.get();
+      for (let index = min; index <= max; index++) {
+        manager.setVar(symbol, new NumberToken(index), false);
+        body.calculate();
+      }
+      return VarManager.unit;
+    })
+  );
+  module.set(
+    "upto_fun",
+    FunToken.native((toks) => {
+      const min = toks[0].request("number");
+      const max = toks[1].request("number");
+      const f = toks[2].request("fun");
+
       for (let index = min; index <= max; index++) {
         f([new NumberToken(index)]);
       }
@@ -33,11 +50,28 @@ const loopInit = () => {
     })
   );
   module.set(
-    'downto',
+    "downto",
     FunToken.native((toks) => {
-      const max = toks[0].request('number');
-      const min = toks[1].request('number');
-      const f = toks[2].request('fun');
+      const max = toks[0].request("number");
+      const min = toks[1].request("number");
+      const symbol = toks[2].request("symbol");
+      const body = toks[3];
+
+      const manager = VarManager.get();
+      for (let index = max; index >= min; index--) {
+        manager.setVar(symbol, new NumberToken(index), false);
+        body.calculate();
+      }
+      return VarManager.unit;
+    })
+  );
+  module.set(
+    "downto_fun",
+    FunToken.native((toks) => {
+      const max = toks[0].request("number");
+      const min = toks[1].request("number");
+      const f = toks[2].request("fun");
+
       for (let index = max; index >= min; index--) {
         f([new NumberToken(index)]);
       }
@@ -45,10 +79,26 @@ const loopInit = () => {
     })
   );
   module.set(
-    'times',
+    "times",
     FunToken.native((toks) => {
-      const times = toks[0].request('number');
-      const f = toks[1].request('fun');
+      const times = toks[0].request("number");
+      const symbol = toks[1].request("symbol");
+      const body = toks[2];
+
+      const manager = VarManager.get();
+      for (let index = 0; index < times; index++) {
+        manager.setVar(symbol, new NumberToken(index), false);
+        body.calculate();
+      }
+      return VarManager.unit;
+    })
+  );
+  module.set(
+    "times_fun",
+    FunToken.native((toks) => {
+      const times = toks[0].request("number");
+      const f = toks[1].request("fun");
+
       for (let index = 0; index < times; index++) {
         f([new NumberToken(index)]);
       }
