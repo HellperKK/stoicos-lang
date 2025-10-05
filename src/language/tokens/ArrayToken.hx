9b4cdfb752d446afff1592a828687c60cb6bc473
Adrien Baudet <baudetad@gmail.com>
2025-10-05 20:16:13 +0200

    made array printable

--- src/language/tokens/ArrayToken.hx
+++ src/language/tokens/ArrayToken.hx
@@ -4,4 +4,12 @@ class ArrayToken extends Value {
     public function new(value: Array<Value>) {
         super("array", value);
     }
+
+    public override function request(type:String):Dynamic {
+        if (type == "string") {
+            return '[${this.value.map(val -> val.request("string")).join(" ")}]';
+        }
+
+        return super.request(type);
+    }
 }
