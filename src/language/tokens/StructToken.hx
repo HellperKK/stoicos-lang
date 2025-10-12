80c54f8eca28860890bd8a2ede0c33b651c0659a
Adrien Baudet <baudetad@gmail.com>
2025-10-12 14:44:18 +0200

    override request("string")

--- src/language/tokens/StructToken.hx
+++ src/language/tokens/StructToken.hx
@@ -4,4 +4,14 @@ class StructToken extends Value {
     public function new(value: Map<String, Value>) {
         super("struct", value);
     }
+    
+    public override function request(type:String):Dynamic {
+        if (type == "string") {
+            var map:Map<String, Value> = this.value;
+            var pairs = [for (kv in map.keyValueIterator()) '${kv.key}:${kv.value.request("string")}'];
+            return '{${pairs.join(" ")}}';
+        }
+
+        return super.request(type);
+    }
 }
--- src/language/tokens/UnitToken.hx
+++ src/language/tokens/UnitToken.hx
@@ -4,4 +4,12 @@ class UnitToken extends Value {
     public function new() {
         super("unit", null);
     }
+    
+    public override function request(type:String):Dynamic {
+        if (type == "string") {
+            return 'Unit';
+        }
+
+        return super.request(type);
+    }
 }
