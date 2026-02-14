80151638e61a3ca598d476c35a682186479a684e
Adrien Baudet <baudetad@gmail.com>
2026-02-14 20:00:17 +0100

    extend module

--- src/language/definitions/FileModule.hx
+++ src/language/definitions/FileModule.hx
@@ -1,5 +1,7 @@
 package language.definitions;
 
+import sys.FileSystem;
+import language.tokens.BooleanToken;
 import sys.io.File;
 import language.tokens.StringToken;
 import language.managers.VarManager;
@@ -10,10 +12,31 @@ class FileModule {
 	public static function load() {
 		var module = new Map<String, Value>();
 
+		module.set("exists", new FunctionToken((values) -> {
+			var path = values[0].request("string");
+
+			return new BooleanToken(FileSystem.exists(path));
+		}, 1));
+
+		module.set("is_directory", new FunctionToken((values) -> {
+			var path = values[0].request("string");
+
+			return new BooleanToken(FileSystem.isDirectory(path));
+		}, 1));
+
         module.set("get_content", new FunctionToken((values) -> {
 			var path = values[0].request("string");
 
 			return new StringToken(File.getContent(path));
+		}, 1));
+
+        module.set("save_content", new FunctionToken((values) -> {
+			var path = values[0].request("string");
+			var content = values[1].request("string");
+
+			File.saveContent(path, content);
+
+			return VarManager.unit;
 		}, 2));
 
 		return new StructToken(module);
