81ef473dd9a17e09168e053fae9cf36a35324fa7
Adrien Baudet <baudetad@gmail.com>
2026-03-10 20:34:42 +0100

    format code

--- src/Main.hx
+++ src/Main.hx
@@ -5,29 +5,25 @@ import language.Parser;
 import sys.io.File;
 
 class Main extends mcli.CommandLine {
-    public function help()
-	{
+	public function help() {
 		Sys.println(this.showUsage());
 		Sys.exit(0);
 	}
 
-	public function run(fileName:String)
-	{
-        var code = File.getContent(fileName);
-        var tokens = Parser.parse(code);
+	public function run(fileName:String) {
+		var code = File.getContent(fileName);
+		var tokens = Parser.parse(code);
 		Prelude.load();
-        for (token in tokens) {
-            token.getValue();
-        }
+		for (token in tokens) {
+			token.getValue();
+		}
 	}
 
 	public function version() {
 		Sys.println("Stoicos version 1.2.0");
 	}
 
-	public static function main()
-	{
+	public static function main() {
 		new mcli.Dispatch(Sys.args()).dispatch(new Main());
 	}
 }
-
