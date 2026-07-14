9a7209fd4f2b3958c33dc44eb09bc1c3fb7b1836
HellperKK <baudetad@gmail.com>
2026-07-14 17:06:55 +0200

    added path resolution to file module

--- src/language/definitions/FileModule.hx
+++ src/language/definitions/FileModule.hx
@@ -1,5 +1,6 @@
 package language.definitions;
 
+import language.managers.PathManager;
 import sys.FileSystem;
 import language.tokens.BooleanToken;
 import sys.io.File;
@@ -13,25 +14,25 @@ class FileModule {
 		var module = new Map<String, Value>();
 
 		module.set("exists", new FunctionToken((values) -> {
-			var path = values[0].request("string");
+			var path = PathManager.get().resolvePath(values[0].request("string"));
 
 			return new BooleanToken(FileSystem.exists(path));
 		}, 1));
 
 		module.set("is_directory", new FunctionToken((values) -> {
-			var path = values[0].request("string");
+			var path = PathManager.get().resolvePath(values[0].request("string"));
 
 			return new BooleanToken(FileSystem.isDirectory(path));
 		}, 1));
 
         module.set("get_content", new FunctionToken((values) -> {
-			var path = values[0].request("string");
+			var path = PathManager.get().resolvePath(values[0].request("string"));
 
 			return new StringToken(File.getContent(path));
 		}, 1));
 
         module.set("save_content", new FunctionToken((values) -> {
-			var path = values[0].request("string");
+			var path = PathManager.get().resolvePath(values[0].request("string"));
 			var content = values[1].request("string");
 
 			File.saveContent(path, content);
