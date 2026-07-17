c507b54e60a979b1cf496f480470c6418722729e
HellperKK <baudetad@gmail.com>
2026-07-17 12:44:34 +0200

    format code

--- src/Main.hx
+++ src/Main.hx
@@ -6,30 +6,26 @@ import language.Parser;
 import sys.io.File;
 
 class Main extends mcli.CommandLine {
-    public function help()
-	{
+	public function help() {
 		Sys.println(this.showUsage());
 		Sys.exit(0);
 	}
 
-	public function run(fileName:String)
-	{
-        var code = File.getContent(fileName);
+	public function run(fileName:String) {
+		var code = File.getContent(fileName);
 		PathManager.get().addPath(fileName);
-        var tokens = Parser.parse(code);
+		var tokens = Parser.parse(code);
 		Prelude.load();
-        for (token in tokens) {
-            token.getValue();
-        }
+		for (token in tokens) {
+			token.getValue();
+		}
 	}
 
 	public function version() {
 		Sys.println("Stoicos version 1.2.0");
 	}
 
-	public static function main()
-	{
+	public static function main() {
 		new mcli.Dispatch(Sys.args()).dispatch(new Main());
 	}
 }
-
